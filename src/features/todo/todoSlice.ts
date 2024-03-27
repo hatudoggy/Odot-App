febf742fffaff9eaf1d60dacdd71ea1e1eeb6728
Morfusee <markvalenzuela.coding@gmail.com>
2024-03-27 20:44:57 +0800

    FEAT: Implement functionality for handling and deleting of selected To Dos

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -3,11 +3,13 @@ import { TodoItem, todoFilters } from "../../interface/ITodo";
 
 export interface TodoState {
   todoList: TodoItem[];
+  selectedTodo: TodoItem[];
   activeTodo: todoFilters;
 }
 
 const initialState: TodoState = {
   todoList: [],
+  selectedTodo: [],
   activeTodo: "all",
 };
 
@@ -27,12 +29,21 @@ export const navigationSlice = createSlice({
         status: action.payload.status,
       });
     },
+    addSelectedTodo: (state, action) => {
+      state.selectedTodo.push(action.payload);
+    },
+    deleteSelectedTodo: (state, action) => {
+      state.selectedTodo = state.selectedTodo.filter(
+        (item) => item.id !== action.payload.id
+      );
+    },
     changeActiveTodo: (state, action) => {
       state.activeTodo = action.payload;
     },
   },
 });
 
-export const { addTodo } = navigationSlice.actions;
+export const { addTodo, addSelectedTodo, deleteSelectedTodo } =
+  navigationSlice.actions;
 
 export default navigationSlice.reducer;
--- src/pages/Schedule/ToDoTab.tsx
+++ src/pages/Schedule/ToDoTab.tsx
@@ -2,16 +2,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { todoFilters } from "../../interface/ITodo";
 import { AnimatePresence, motion } from "framer-motion";
-import { MdOutlineAdd } from "react-icons/md";
 import { dateTimeFormatter } from "../../hooks/reusable code/dateFormatter";
-import { openModal } from "../../features/modal/modalSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // DB
 import { db, TodoItem } from "../../localdb/db";
 
 // Hooks
 import { useLiveQuery } from "dexie-react-hooks";
+
+// React icons
+import { MdDelete } from "react-icons/md";
+import { MdOutlineAdd } from "react-icons/md";
+import { IoAccessibility } from "react-icons/io5";
+
+// Redux
+import {
+  addSelectedTodo,
+  deleteSelectedTodo,
+} from "../../features/todo/todoSlice";
+import { openModal } from "../../features/modal/modalSlice";
 
 function ToDoTab() {
   return (
@@ -69,6 +79,27 @@ function ToDoFilters() {
     dispatch({ type: "todo/changeActiveTodo", payload: newActiveTodo });
   };
 
+  type attributeType = "css" | "onclick";
+
+  /**
+   * Provides an onClick event for the delete button or
+   * a CSS class for the delete button
+   *
+   * @param attribute - "css" or "onclick" to determine the return value
+   * @returns - A string or void
+   * */
+  const checkedItemsPresent = (attribute: attributeType): string | void => {
+    if (todoState.selectedTodo.length !== 0 && attribute === "onclick") {
+      dispatch(
+        openModal({ modal: "todoDelete", modalPayload: todoState.selectedTodo }) // Item to delete
+      );
+      return;
+    }
+
+    if (todoState.selectedTodo.length === 0 && attribute === "css")
+      return "opacity-60 cursor-not-allowed";
+  };
+
   return (
     <div className="flex gap-3 py-5 items-center">
       {filters.map((filter, index) => (
@@ -99,11 +130,20 @@ function ToDoFilters() {
           </motion.button>
         </AnimatePresence>
       ))}
-      <MdOutlineAdd
-        className="ml-auto w-8 h-8 p-1 rounded-md cursor-pointer"
-        style={{ backgroundColor: "rgba(100, 125, 126, 1)" }}
-        onClick={() => dispatch(openModal({ modal: "todoAdd" }))}
-      />
+      <span className="ml-auto flex gap-2">
+        <MdDelete
+          className={
+            `transition-all ml-auto bg-red-500 bg-opacity-70 px-1 rounded-md h-8 w-8 font-medium cursor-pointer ` +
+            checkedItemsPresent("css")
+          }
+          onClick={() => checkedItemsPresent("onclick")}
+        />
+        <MdOutlineAdd
+          className="ml-auto w-8 h-8 p-1 rounded-md cursor-pointer"
+          style={{ backgroundColor: "rgba(100, 125, 126, 1)" }}
+          onClick={() => dispatch(openModal({ modal: "todoAdd" }))}
+        />
+      </span>
     </div>
   );
 }
@@ -121,6 +161,33 @@ function ToDoCard() {
   const todoQuery = useLiveQuery(() => db.todo.toArray(), [todoState]);
   const dispatch = useDispatch();
 
+  const handleSelectTodo = (
+    e: React.MouseEvent<HTMLInputElement>,
+    todo: TodoItem
+  ) => {
+    // Serialize the todo item to make it serializable
+    const serializedTodo = {
+      id: todo?.id,
+      title: todo?.title,
+      startDate: todo?.startDate.toISOString(), // Fixes the non-serializable error
+      endDate: todo?.endDate.toISOString(),
+      tags: todo?.tags,
+      priority: todo?.priority,
+      note: todo?.note,
+      status: todo?.status,
+    };
+
+    if (e.currentTarget.checked) {
+      dispatch(addSelectedTodo({ serializedTodo }));
+      return;
+    }
+
+    // If the selected todo is unchecked, remove it from the selected todo list
+    dispatch(deleteSelectedTodo({ serializedTodo }));
+  };
+
+  // console.log(todoState.selectedTodo);
+
   return (
     <>
       {todoQuery &&
@@ -130,7 +197,11 @@ function ToDoCard() {
             style={{ backgroundColor: "rgba(100, 125, 126, 0.25)" }}
             key={index + todo.title}
           >
-            <input type="checkbox" className="w-4 h-4" />
+            <input
+              type="checkbox"
+              className="w-4 h-4"
+              onClick={(e) => handleSelectTodo(e, todo)}
+            />
             <section className="px-1.5 truncate w-[90%]">
               <h1 className="text-lg font-semibold truncate max-w-[48vw]">
                 {todo.title}
